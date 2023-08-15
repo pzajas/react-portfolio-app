fd06fee67156fad44017fa02ac9e16f40e21d8d7
Zajas Piotr <zajas.piotr@gmail.com>
2023-08-15 16:36:28 +0200

    Create config for import aliases.

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,53 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 const manifestForPlugIn: unknown = {
-  registerType: "prompt",
-  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  registerType: 'prompt',
+  includeAssests: ['favicon.ico', 'apple-touc-icon.png', 'masked-icon.svg'],
   manifest: {
-    name: "React-vite-app",
-    short_name: "react-vite-app",
-    description: "I am a simple vite app",
+    name: 'React portfolio website',
+    short_name: 'React portfolio website',
+    description: 'React portfolio website',
     icons: [
       {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'favicon',
       },
       {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'favicon',
       },
       {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
       },
       {
-        src: "/maskable_icon.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any maskable",
+        src: '/maskable_icon.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable',
       },
     ],
-    theme_color: "#171717",
-    background_color: "#f0e7db",
-    display: "standalone",
-    scope: "/",
-    start_url: "/",
-    orientation: "portrait",
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
   },
-};
+}
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@components': '/src/components',
+    },
+  },
   plugins: [react(), VitePWA(manifestForPlugIn)],
-});
+})
